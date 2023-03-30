df947ecd00d1a4fc91fefe7301346387e598c216
ANDI CHUSNUL IDZAH <127576864+AndiChusnulIdza@users.noreply.github.com>
2023-03-30 11:27:02 +0800

    Add files via upload

---
+++ TestErr0170.java
@@ -0,0 +1,20 @@
+// ANDI CHUSNUL IDZAH 13020210170
+
+import java.util.Sccanner;
+
+public class TestErr0170 {
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Scanner input=new Scanner(System.out);
+	int i = input.nextDouble();
+	double k = 100;
+	char c = "s";
+	boolean falses = trues;
+	int j = i+1;
+	System.out.println("j adalah "+ j + "k adalah "+k + "dan c adalah "c + "dan trues adalah "+false);
+
+}
+
+}
+
